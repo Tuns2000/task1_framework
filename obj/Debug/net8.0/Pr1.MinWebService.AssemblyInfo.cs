3077422ee953af82a14550a58e209001a77f589b
Arseniy <arseniyb.2005@gmail.com>
2026-02-20 17:12:24 +0300

    Remove Read

--- obj/Debug/net8.0/Pr1.MinWebService.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pr1.MinWebService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pr1.MinWebService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b17ed66e2ba3cd8fbc8fca5b82b69c2e6933e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ff7f83d3f3b0c4eb0f930488463bb5fde56fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pr1.MinWebService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pr1.MinWebService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
